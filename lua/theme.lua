52574658c97e6605c0a3c2ed5046bb0366d83db9
Tyler Gregg <tyler.gregg.ma@gmail.com>
2023-03-20 00:19:49 -0600

    fixed transparent

--- lua/theme.lua
+++ lua/theme.lua
@@ -21,6 +21,12 @@ vim.cmd("colorscheme carbonfox")
 vim.g.lightline = { colorscheme = 'carbonfox' }
 
 transparent.setup({
+    groups = { -- table: default groups
+    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+    'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+  },
   extra_groups = { -- table/string: additional groups that should be cleared
     -- In particular, when you set it to 'all', that means all available groups
 
@@ -32,7 +38,9 @@ transparent.setup({
     "BufferLineSeparator",
     "BufferLineIndicatorSelected",
   },
+  exclude_groups = {},
 })
 
+vim.cmd("g:transparent_enabled=true")
 -- vim.cmd("colorscheme gruvbox")
 -- vim.cmd("let g:gruvbox_contrast_dark = 'hard'")
--- lua/window.lua
+++ lua/window.lua
@@ -10,28 +10,28 @@ vim.api.nvim_create_autocmd("WinLeave", {
     command = "set nocursorline",
 })
 
-vim.cmd([[
-"
-highlight ColorColumn guibg=#17161D
-if exists('+colorcolumn')
-  function! s:DimInactiveWindows()
-    for i in range(1, tabpagewinnr(tabpagenr(), '$'))
-      let l:range = ''
-      if i != winnr()
-        if &wrap
-         let l:width=256 
-        else
-         let l:width=winwidth(i)
-        endif
-        let l:range = join(range(1, l:width), ',')
-      endif
-      call setwinvar(i, '&colorcolumn', l:range)
-    endfor
-  endfunction
-  augroup DimInactiveWindows
-    au!
-    au WinEnter * call s:DimInactiveWindows()
-  augroup END
-endif
-"
-]])
+-- vim.cmd([[
+-- "
+-- highlight ColorColumn guibg=#17161D
+-- if exists('+colorcolumn')
+--   function! s:DimInactiveWindows()
+--     for i in range(1, tabpagewinnr(tabpagenr(), '$'))
+--       let l:range = ''
+--       if i != winnr()
+--         if &wrap
+--          let l:width=256 
+--         else
+--          let l:width=winwidth(i)
+--         endif
+--         let l:range = join(range(1, l:width), ',')
+--       endif
+--       call setwinvar(i, '&colorcolumn', l:range)
+--     endfor
+--   endfunction
+--   augroup DimInactiveWindows
+--     au!
+--     au WinEnter * call s:DimInactiveWindows()
+--   augroup END
+-- endif
+-- "
+-- ]])
